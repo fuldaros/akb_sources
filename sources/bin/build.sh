55085dfdcb742c41d64f3b187b2ef8d7347e07b4
fuldaros <litefros@gmail.com>
2023-07-22 20:09:12 +0300

    upds

--- sources/bin/build.sh
+++ sources/bin/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # by xyzmean
+
 ## FUNCTIONS START
 # CLEAN TMP
 export a1=$(sed -n 2p make.prop)
@@ -15,6 +16,7 @@ export a10=$(sed -n 14p make.prop)
 export a11=$(sed -n 18p make.prop)
 export a12=$(sed -n 16p make.prop)
 export a13="1"
+
 # Проверка make.prop на кол-во строк
 ch1=$(sed -n 19p make.prop)
 if [ "$ch1" != "" ]; then
@@ -28,25 +30,25 @@ else
     rm -f author.prop
   }
 
-  # Не ебу зачем но второй раз вводим переменные
+  # Необходимо явно объявлять переменные с помощью local
   function linkvar() {
-    usr=$a1
-    bh=$a2
-    arch=$a3
-    stamp=$a4
-    stampt=$a5
-    logb=$a6
-    otazip=$a7
-    device=$a8
-    cpu=$a9
-    imgt=$a10
-    loc=$a11
-    gcc=$a12
-    sha=$a13
-    archp=$(dpkg --print-architecture)
-    cpus=$(cat /proc/cpuinfo | grep processor | wc -l)
-    th=$(($cpus + 1)) 
- }
+    local usr=$a1
+    local bh=$a2
+    local arch=$a3
+    local stamp=$a4
+    local stampt=$a5
+    local logb=$a6
+    local otazip=$a7
+    local device=$a8
+    local cpu=$a9
+    local imgt=$a10
+    local loc=$a11
+    local gcc=$a12
+    local sha=$a13
+    local archp=$(dpkg --print-architecture)
+    local cpus=$(cat /proc/cpuinfo | grep processor | wc -l)
+    local th=$(($cpus + 1))
+  }
 
   # Экспорт необходимых данных для сборки
   function exportmkval() {
@@ -61,13 +63,13 @@ else
     echo -e "$g Собираем OTA пакет...$y"
     cat out/kernel/"$kernel" > otagen/zImage
     cd otagen
-    echo "ZIP file is generated automatically by fuldaros's script on "$stamp""\n"Good luck!" > generated.info
+    echo "ZIP file is generated automatically by fuldaros's script on "$stamp""$'\n'"Good luck!" > generated.info
     echo -e "$g Генерируем author.prop...$y"
     sleep 2
-    cat ../make.prop >author.prop
-    echo -e "# BUILD TIME" "\n""$stampt" >> author.prop
-    echo -e "# AKB ver. (DONT EDIT)""\n""$ver" >> author.prop
-    echo -e "#BUILD TYPE" "\n""$type" >> author.prop
+    cat ../make.prop > author.prop
+    echo -e "# BUILD TIME"$'\n'"$stampt" >> author.prop
+    echo -e "# AKB ver. (DONT EDIT)"$'\n'"$ver" >> author.prop
+    echo -e "# BUILD TYPE"$'\n'"$type" >> author.prop
     echo -e "$g Сжимаем...$y"
     sleep 3
     zip -r ../out/ota/"$otazip".zip *
@@ -77,19 +79,20 @@ else
   function printinfo() {
     echo -e "$cy******************************$y"
     echo -e "$g   Build info"
-    echo -e "$y    User: "$usr"
-    Host: "$bh"
-    ARCH: "$arch"
-    CPU: "$cpu"
-    Device: "$device"
-    Build time: "$stamp"
-    Kernel location: "$loc"
-    Build type: "$type"
-    Threads: "$th""
+    echo -e "$y    User: $usr"
+    echo -e "    Host: $bh"
+    echo -e "    ARCH: $arch"
+    echo -e "    CPU: $cpu"
+    echo -e "    Device: $device"
+    echo -e "    Build time: $stamp"
+    echo -e "    Kernel location: $loc"
+    echo -e "    Build type: $type"
+    echo -e "    Threads: $th"
     echo -e "$cy******************************$y"
   }
 
   ## FUNCTIONS END
+
   ver=1.5-nightly
   clear
   e="\x1b["
@@ -99,67 +102,89 @@ else
   r=$e"1;91m"
   g=$e"92;01m"
   m=$e"95;01m"
-  echo -e "
-    $cy****************************************************
-    $cy*       Automatic kernel builder v"$ver"      *
-    $cy*                   by xyzmean                     *
-    $cy****************************************************
-    $y"
+  echo -e "$cy****************************************************
+$cy*           Automatic kernel builder v"$ver"          *
+$cy*                   by xyzmean                    *
+$cy****************************************************
+$y"
   sleep 3
-  # Прерываем выполнение при появлении пошибки
+ 
+  # Прерывание выполнения при возникновении ошибки
   set -e
+ 
   # Создание переменных
   linkvar
+ 
   # Тип сборки (бесполезная хрень)
   if [[ "$sha" != "1" ]]; then
     type="USER"
   else
     type="OFFICIAL"
   fi
+ 
   # Еще переменная
   kernel="$imgt"_"$stamp"
-  # Экспортируем необходимые значения из make.prop
+ 
+  # Экспорт необходимых данных для сборки
   exportmkval
+ 
   printinfo
+ 
   sleep 4
-  # Экспортируем gcc из make.prop
-#  if [["$archp" = "amd64"]]; then
+ 
+  # Экспорт gcc из make.prop
+  if [[ "$archp" = "amd64" ]]; then
     export CROSS_COMPILE="$PWD"/gcc/bin/"$gcc"
-#  else 
-#    echo "Your arch not amd64!";
-#  fi
+  else 
+    echo "Your arch not amd64!"
+  fi
+ 
   cd sources/
-  echo -e "$g Начинаем сборку ядра...$y"
+ 
+  echo -e "$g Building the kernel...$y"
+ 
   strt=$(date +"%s")
-  # Сборка ядра :3
+ 
+  # Сборка ядра
   make -j$th O=../out/build/"$device" "$imgt"
+ 
   clear
-  # Тайтл завершения сборки
+ 
+  # Заголовок завершения сборки
   echo -e "
 $cy****************************************************
 $cy*           Automatic kernel builder v"$ver"          *
 $cy*                   by xyzmean                    *
 $cy****************************************************
-    $y"
-  echo -e "$g Сборка завершена!
-    Переносим ядро в out/kernel... $y"
+$y"
+ 
+  echo -e "$g Build completed!
+    Transferring the kernel to out/kernel... $y"
+ 
   sleep 3
+ 
   # Перенос ядра в папку out/kernel
   cat ../out/build/"$device"/arch/"$arch"/boot/"$imgt" >../out/kernel/"$kernel"
   rm -rf ../out/build/"$device"/arch/"$arch"/boot/
   cd ../
   mkota
-  echo -e "$g Удаление временных фаилов...$y"
-  # Отчистка tmp фаилов
+ 
+  echo -e "$g Removing temporary files...$y"
+ 
+  # Очистка временных файлов
+ 
   sleep 1
   cleantmp
-  # Вывод имени собранного пакета
-  echo -e "$g Готово! Имя OTA пакета: "$otazip".zip$y"
+ 
+  # Вывод имени созданного пакета
+  echo -e "$g Done! OTA package name: "$otazip".zip$y"
+ 
   # Генерация и вывод времени сборки
   end=$(date +"%s")
   diff=$(($end - $strt))
-  echo Операция выполнена успешно!
+  echo Operation completed successfully!
   sleep 2
-  echo -e "$m Компиляция заняла "$diff" секунд!"
+  echo -e "$m Compilation took $diff seconds!"
 fi
+
 ####### script v12 (beta)
